efe02a48bdb30e2dd91ecbdd5caaa4e29cb76a45
leye849 <iw540317@gmail.com>
2026-06-21 16:39:35 +0900

    Start map from current location

--- components/KakaoMap.tsx
+++ components/KakaoMap.tsx
@@ -227,5 +227,11 @@ export default function KakaoMap({
     map.panTo(new window.kakao.maps.LatLng(focus.lat, focus.lng));
   }, [focus]);
 
+  useEffect(() => {
+    const map = mapRef.current;
+    if (!map || !window.kakao?.maps || !center) return;
+    map.panTo(new window.kakao.maps.LatLng(center.lat, center.lng));
+  }, [center]);
+
   return <div ref={ref} className="h-full w-full" />;
 }
